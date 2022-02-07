2e0da72c42713a9bc5d73b8cf07f67463fd3ef04
Elias Dillinger <84124876+Elektrominati@users.noreply.github.com>
2022-02-07 22:26:16 +0100

    test_UART2_output
    just a test if i can print Integers in my Serial Console

--- IMB/KEIL_prog/main.c
+++ IMB/KEIL_prog/main.c
@@ -79,6 +79,7 @@ void TIM2_IRQHandler(void) {
 	pulseCount += 1;//raise the value every time the Handler starts-> every 1ms
 	if(pulseCount == 80 - 1){//after 4ms-> 80-1 because we start to count form 0
 		GPIOA->BSRR = 0x00000020;   /* turn on LED */
+		USART2_write(pulseCount);
 	}
 	if(pulseCount == 100 - 1){//after 5ms-> 100-1 because we start to count form 0
 		GPIOA->BSRR = 0x00200000;   /* turn off LED */
@@ -87,6 +88,30 @@ void TIM2_IRQHandler(void) {
 		TIM2->CR1 = 0x0000; //DISABLE the Timer, so we can Synch & Enable the Timer with the next EXTI
 	}
 	
+}
+/*----------------------------------------------------------------------------
+  Initialize UART pins, Baudrate
+ *----------------------------------------------------------------------------*/
+void USART2_init (void) {
+    RCC->AHB1ENR |= 1;          /* Enable GPIOA clock */
+    RCC->APB1ENR |= 0x20000;    /* Enable USART2 clock */
+
+    /* Configure PA2 for USART2_TX */
+    GPIOA->AFR[0] &= ~0x0F00;
+    GPIOA->AFR[0] |=  0x0700;   /* alt7 for USART2 */
+    GPIOA->MODER  &= ~0x0030;
+    GPIOA->MODER  |=  0x0020;   /* enable alternate function for PA2 */
+
+    USART2->BRR = 0x0683;       /* 9600 baud @ 16 MHz */
+    USART2->CR1 = 0x0008;       /* enable Tx, 8-bit data */
+    USART2->CR2 = 0x0000;       /* 1 stop bit */
+    USART2->CR3 = 0x0000;       /* no flow control */
+    USART2->CR1 |= 0x2000;      /* enable USART2 */
+}
+/* Write a character to USART2 */
+void USART2_write (int ch) {
+    while (!(USART2->SR & 0x0080)) {}   // wait until Tx buffer empty
+    USART2->DR = (ch & 0xFF);
 }
 
 
