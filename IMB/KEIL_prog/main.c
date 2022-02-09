2c2f199565f7ea2a592f7840eb018a066bb9a915
Elias Dillinger <84124876+Elektrominati@users.noreply.github.com>
2022-02-09 23:21:08 +0100

    UART2_interval_1Hz_TIM3
    Every Second the Shell should try to print out the pulseCount Value [int], but it can't be resolved.
    It needs to be converted to a Char Array

--- IMB/KEIL_prog/main.c
+++ IMB/KEIL_prog/main.c
@@ -31,6 +31,14 @@ int main(void) {
     /* configure PA5 for LED */
     GPIOA->MODER &= ~0x00000C00;        /* clear pin mode */
     GPIOA->MODER |=  0x00000400;        /* set pin to output mode */
+	
+	
+    // configure TIM3 to wrap around at 1 Hz
+    RCC->APB1ENR |= 2;              /* enable TIM3 clock */
+    TIM3->PSC = 1600 - 1;           /* divided by 1600 */
+    TIM3->ARR = 10000 - 1;          /* divided by 10000 */
+    TIM3->CNT = 0;                  /* clear timer counter */
+    TIM3->CR1 = 1;                  /* enable TIM3 */
 
     /* configure PC13 for push button interrupt */
     GPIOC->MODER &= ~0x0C000000;        /* clear pin mode to input mode */
@@ -47,6 +55,9 @@ int main(void) {
     __enable_irq();                     /* global enable IRQs */
     
     while(1) {
+	while(!(TIM3->SR & 1)) {}   /* wait until UIF set TIM3 */
+        TIM3->SR &= ~1;             /* clear UIF TIM3*/
+	printf(pulseCount);
     }
 }
 
